95c7e72ff22ed4b38cf514deb5e80f98f3e75eb2
Bunny <abhi3447141@gmail.com>
2024-02-10 20:23:37 +0530

    Join Page CSS push

--- src/Component/Join/Join.jsx
+++ src/Component/Join/Join.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import './Join.css';
 import logo from "../../Assets/png/logo-no-background.png"
+import {Link} from "react-router-dom";
 
+let user;
+ 
 const Join = () => {
+
+  const sendUser = () =>{
+    user = document.getElementById('JoinInput').value
+  }
   return (
    <div className="JoinPage">
      <div className="JoinContainer">
        <img src={logo} alt="logo"/>
        <h1>Chatter-Box</h1>
+       <input type="text" placeholder="Hi My name is ChatterBox. What's yours? " id="JoinInput" />
+       <Link style={{textDecoration: "none"}} to="/chat">
+       <button className='joinBTN' onClick={sendUser}>WELCOME</button>
+       </Link>
      </div>
    </div>
   )
